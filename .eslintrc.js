fdc1148f9a252ceb55c7dab960e862d23f8cdf3d
Antoni Silvestrovič <antoni.silvestrovic@gmail.com>
2021-12-17 13:51:07 +0200

    Added literally everything

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+};
